edb792afcb6b3245899d8d60bad9869312eb6f0d
jiei0833 <251zs01757@student.zen.ac.jp>
2025-11-26 19:46:42 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -19,15 +19,29 @@ assessmentButton.addEventListener(
 
   //診断結果表示エリアの作成
   resultDivision.innerText = ''
-  const header = document.createElement('h3');
-  header.innerText = '診断結果'
-  resultDivision.appendChild(header);
+
+  //headerDivision の作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class','card-header text-bg-primary' );
+  headerDivision.innerText = '診断結果';
+  
+  //badyDivision の作成
+  const bodyDivision =document.createElement('div');
+  bodyDivision.setAttribute('class', 'card-body');
 
   const paragraph = document .createElement('p')
+  paragraph.setAttribute('class', 'card=text')
   const result = assessment(userName)
   paragraph.innerText = result;
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
 
+  //resultDivision に　bootstrap　のスタイルを適用する
+  resultDivision.setAttribute('class','card')
+
+  //headerDivision と bodyDIvision を　resultDivision　に差し込む
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
+  
     // ツイートエリアの作成
     tweetDivision.innerText = '';
     const anchor = document.createElement('a');
@@ -37,7 +51,7 @@ assessmentButton.addEventListener(
       '&ref_src=twsrc%5Etfw';
     anchor.setAttribute('href', hrefValue)
     anchor.setAttribute('class','twitter-hashtag-button');
-    anchor.setAttribute('date-text','診断結果の文章');
+    anchor.setAttribute('data-text','診断結果の文章');
     anchor.innerText = 'Tweet #あなたのいいところ';
 
     tweetDivision.appendChild(anchor);
